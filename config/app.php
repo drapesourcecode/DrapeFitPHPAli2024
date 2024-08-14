97ac68ecd246e57fc25bd1bfa76d73f25e0480fd
drapesourcecode <52990960+drapesourcecode@users.noreply.github.com>
2024-08-14 14:40:32 +0530

    Update app.php

--- config/app.php
+++ config/app.php
@@ -303,7 +303,7 @@
            'className' => 'Cake\Database\Connection',
            'driver' => 'Cake\Database\Driver\Mysql',
            'persistent' => false,
-            'host' => '108.167.139.168',
+            'host' => '104.131.0.179',
            /**
             * CakePHP will use the default DB port based on the driver selected
             * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
