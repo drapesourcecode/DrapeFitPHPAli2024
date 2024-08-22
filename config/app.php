c61c0c7ad04dbb32e6c1b65b3a7c000d65fa9447
drapesourcecode <52990960+drapesourcecode@users.noreply.github.com>
2024-08-22 12:28:13 +0530

    Update app.php

--- config/app.php
+++ config/app.php
@@ -22,7 +22,7 @@
       * Development Mode:
       * true: Errors and warnings shown.
       */
-     'debug' => filter_var(env('DEBUG',true), FILTER_VALIDATE_BOOLEAN),
+     'debug' => filter_var(env('DEBUG',false), FILTER_VALIDATE_BOOLEAN),
      /**
       * Configure basic information about the application.
       *
